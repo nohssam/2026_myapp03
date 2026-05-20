24f7d2da2f6bf2dd732f5f3193a0fb3abfcef4dc
nohssam <nohssam73@gmail.com>
2026-05-20 10:35:49 +0900

    05-20-1

--- src/api/Auth.jsx
+++ src/api/Auth.jsx
@@ -10,3 +10,28 @@ import { api } from "./Http";
 
 export const register = (member) =>
     api.post('/members/register', member)
+
+
+// 요청 인터셉터 
+api.interceptors.request.use(
+    (config) => {
+        // 토큰을 붙이지 않아도 되는 경로 
+        const exludePaths = [
+            '/members/login',
+            '/members/register',
+            '/members/refresh'
+        ]
+
+        // 위에서 지정한 엔드포인트를 제외하고 
+        const isExcluded = exludePaths.some((path)=> config.url.includes(path))
+
+        if(isExcluded){
+            // localStorage에서 토큰 읽
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
+// 응답 인터셉터
+api.interceptors.response.use()
