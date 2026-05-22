c44d988e6bc40c74a5d719fa84a26942d5d0681f
nohssam <nohssam73@gmail.com>
2026-05-22 13:15:38 +0900

    guestbookupdate

--- src/api/GuestBook.jsx
+++ src/api/GuestBook.jsx
@@ -2,3 +2,9 @@ import {api} from "./Http"
 
 export const guestbookList = () =>
     api.get("/guestbook/list")
+
+export const guestbookInsert = (gvo) =>
+    api.post("/guestbook/insert", gvo)
+
+export const guestbookUpdate = (gvo) =>
+    api.post("/guestbook/update" ,gvo)
